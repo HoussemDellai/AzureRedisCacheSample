95d6ecb605f6421b1b907d2fef200d90f6cc52c4
Houssem Dellai <hodellai@microsoft.com>
2020-02-20 09:59:21 +0100

    first commit

--- AzureRedisCacheConsoleDemo/Program.cs
+++ AzureRedisCacheConsoleDemo/Program.cs
@@ -5,6 +5,7 @@ namespace AzureRedisCacheConsoleDemo
 {
     class Program
     {
+
         private static Lazy<ConnectionMultiplexer> _lazyConnection = new Lazy<ConnectionMultiplexer>(
             () => { return ConnectionMultiplexer.Connect("senelec.redis.cache.windows.net:6380,password=nJWmd4RdwHfM6ww1bhsgfHcm+ZJiurSucfGOThw8NU0=,ssl=True,abortConnect=False"); }
         );

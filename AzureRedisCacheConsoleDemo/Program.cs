75b450a6a21b449f82b04a51c0bc95df35786ed2
Houssem Dellai <hodellai@microsoft.com>
2020-02-20 09:59:00 +0100

    Add project files.

---
+++ AzureRedisCacheConsoleDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace AzureRedisCacheConsoleDemo
+{
+    class Program
+    {
+        private static Lazy<ConnectionMultiplexer> _lazyConnection = new Lazy<ConnectionMultiplexer>(
+            () => { return ConnectionMultiplexer.Connect("senelec.redis.cache.windows.net:6380,password=nJWmd4RdwHfM6ww1bhsgfHcm+ZJiurSucfGOThw8NU0=,ssl=True,abortConnect=False"); }
+        );
+
+        public static ConnectionMultiplexer Connection => _lazyConnection.Value;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
